097f8df1543007a050cc72a9f02a494d3417a8a1
Gzppp <2213041225@qq.com>
2022-07-03 11:14:54 +0800

    '初始化项目'

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    open: true,
+    host: "localhost",
+    port: 6666,
+  },
 });
